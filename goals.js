cd90a3d9eef23d692c8742e333e83169a32a195c
Jaskarn Kahlon <jaskarnkahlon@res1-185-223-dhcp.int.colorado.edu>
2022-03-05 23:57:55 -0700

    UI update 11:57

---
+++ goals.js
@@ -0,0 +1,3 @@
+document.getElementById("closebtn").addEventListener("click",function(){
+    document.getElementById("closebtn").parentElement.remove();
+})
---
+++ images/algorithms.js
@@ -0,0 +1,39 @@
+function getCharityId(charityCat, charity){
+    switch (charityCat) {
+        //Health and Wellness
+        case 'HW':
+            switch(charity){
+                case "":
+                break;
+                default:
+            }
+        break;
+        //Wildlife
+        case 'wildlife':
+            switch(charity){
+                case "":
+                break;
+                default:
+            }
+        break;
+        //Enviroment
+        case 'enviroment':
+            switch(charity){
+                case "":
+                break;
+                default:
+            }
+        break;
+        //Eductation
+        case 'education':
+            switch(charity){
+                case "":
+                break;
+                default:
+            }
+        break;
+        default:
+      }
+      
+}
+
